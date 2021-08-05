c7682f46db75b60f1235295acdf893c2dcacda2a
Dipali <dipalicthakre@gmail.com>
2021-08-05 16:46:18 +0530

    my first

---
+++ src/main/java/com/dt/Beans/AartGallaeryy.java
@@ -0,0 +1,11 @@
+package com.dt.Beans;
+
+public class AartGallaeryy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi.....");
+
+	}
+
+}
